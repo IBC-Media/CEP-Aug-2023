8391c309eff3ce23e8d270c059a05b10ad258e88
RishinR <rishinrnew2020@gmail.com>
2023-08-17 20:18:53 +0530

    added assignment1 solution

---
+++ assignments/assgn1.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("The table of 9 is: ");
+    // Here we have considered the table of 9 till 20, it could be any times as long as the output is right.
+    for i in 1..21{
+        println!("{i} * 9 = {}", i*9);
+    }
+}
